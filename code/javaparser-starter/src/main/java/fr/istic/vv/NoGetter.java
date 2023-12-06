84937671a977546b8924614465402805665aa593
clara <poncetcla@gmail.com>
2023-12-06 16:07:41 +0100

    exercice 4

--- code/javaparser-starter/src/main/java/fr/istic/vv/NoGetter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/NoGetter.java
@@ -1,20 +1,25 @@
 package fr.istic.vv;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 
 import java.io.*;
 import java.util.*;
 
 public class NoGetter extends VoidVisitorWithDefaults<Void> {
-    private Set<String> attributeNames;
-    private Set<String> getterNames;
-    private Map<String, Set<String>> reportedAttributesPerClass;
+    private final Set<VariableDeclarator> attributes;
+    private final Set<MethodDeclaration> getters;
+    private final Map<String, Set<String>> reportedAttributesPerClass;
 
     public NoGetter() {
-        attributeNames = new HashSet<>();
-        getterNames = new HashSet<>();
+        attributes = new HashSet<>();
+        getters = new HashSet<>();
         reportedAttributesPerClass = new HashMap<>();
     }
 
@@ -25,7 +30,7 @@ public class NoGetter extends VoidVisitorWithDefaults<Void> {
         }
 
         try {
-            FileOutputStream fos = new FileOutputStream(new File("report.md"));
+            FileOutputStream fos = new FileOutputStream("report.md");
             PrintWriter pw = new PrintWriter(fos);
 
             for (String className : reportedAttributesPerClass.keySet()) {
@@ -39,16 +44,15 @@ public class NoGetter extends VoidVisitorWithDefaults<Void> {
 
             pw.close();
             fos.close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("Could not write report.md");
         }
     }
 
     @Override
     public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
-        attributeNames.clear();
-        getterNames.clear();
+        attributes.clear();
+        getters.clear();
 
         for (FieldDeclaration field : declaration.getFields())
             visit(field, arg);
@@ -56,45 +60,71 @@ public class NoGetter extends VoidVisitorWithDefaults<Void> {
             visit(method, arg);
 
         Set<String> reportedAttributes = new HashSet<>();
-        for (String attributeName : attributeNames) {
-            if (getterNames.contains(getGetterName(attributeName))) continue;
-            reportedAttributes.add(attributeName);
+        for (VariableDeclarator attr : attributes) {
+            if (!hasGetter(attr))
+                reportedAttributes.add(attr.getNameAsString());
         }
 
         reportedAttributesPerClass.put(declaration.getNameAsString(), reportedAttributes);
-
-        for (String className : reportedAttributesPerClass.keySet()) {
-            System.out.println("## " + className);
-            System.out.println("The following attributes are not accessed through a getter:");
-            for (String attributeName : reportedAttributesPerClass.get(className)) {
-                System.out.println("* " + attributeName);
-            }
-            System.out.println();
-        }
     }
 
     @Override
     public void visit(FieldDeclaration declaration, Void arg) {
         if (declaration.isPublic()) return;
-        String attributeName = declaration.getVariable(0).getNameAsString();
 
-        System.out.println(attributeName);
-
-        attributeNames.add(attributeName);
+        attributes.addAll(declaration.getVariables());
     }
 
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
-        if (!declaration.isPublic() || !declaration.getParameters().isEmpty()) return;
+        if (!declaration.isPublic()) return;
         String methodName = declaration.getNameAsString();
         if (!methodName.startsWith("get")) return;
 
-        System.out.println(methodName);
-
-        getterNames.add(methodName);
+        getters.add(declaration);
     }
 
-    public String getGetterName(String attributeName) {
+    private boolean hasGetter(VariableDeclarator attr) {
+
+        NodeList<Statement> statements;
+        Statement statement;
+        for (MethodDeclaration getter : getters) {
+            if (isGetter(getter, attr))
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isGetter(MethodDeclaration method, VariableDeclarator attr) {
+        if (!method.getNameAsString().equals(getGetterName(attr.getNameAsString())))
+            return false;
+
+        if (!method.getBody().isPresent())
+            return false;
+
+        BlockStmt body = method.getBody().get();
+
+        if (body.getStatements().size() != 1)
+            return false;
+
+        Statement statement = body.getStatement(0);
+
+        if (!(statement.isReturnStmt()))
+            return false;
+
+        ReturnStmt returnStmt = statement.asReturnStmt();
+
+        if (!returnStmt.getExpression().isPresent())
+            return false;
+
+        Expression expression = returnStmt.getExpression().get();
+
+        return expression.toString().equals(attr.getNameAsString()) ||
+                expression.toString().equals("this." + attr.getNameAsString());
+    }
+
+    private String getGetterName(String attributeName) {
         return "get" + attributeName.substring(0, 1).toUpperCase() + attributeName.substring(1);
     }
 }
